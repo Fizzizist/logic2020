f99c8ef878f0460a59971393e58fdaa669e50658
Peter Vlasveld <pvlasveld@mailfence.com>
2019-09-08 21:13:47 -0400

    FEAT-2: Removed redux from project.

--- logix/assets/logix/index.jsx
+++ logix/assets/logix/index.jsx
@@ -1,17 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/app';
-import store from './store/index';
-import {Provider} from 'react-redux';
 
 ReactDOM.render(
-    <Provider store={store}>
+    <div>
       <link
         rel="stylesheet"
         href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T"
         crossOrigin="anonymous"
       />
       <App />
-    </Provider>,
+    </div>,
     document.getElementById('react')
 );

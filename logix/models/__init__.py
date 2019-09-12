b25c9cdcf6da1f86c3c4c8121c133411c6ba3f16
fizzizist <peter.vlasveld@cyclicarx.com>
2019-09-12 08:00:54 -0400

    FEAT-2:Created Argument and ArgumentUser models.

---
+++ logix/models/__init__.py
@@ -0,0 +1,2 @@
+from logix.models.argument import Argument
+from logix.models.argument_user import ArgumentUser
--- logix/models/argument_user.py
+++ logix/models/argument_user.py
@@ -1,6 +1,11 @@
 from django.db import models
 from django.contrib.auth.models import User
+from logix.models import Argument
 
-class ArgumentUser(models.Model){
-    
-}
+
+class ArgumentUser(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    argument = models.ForeignKey(Argument, on_delete=models.CASCADE)
+
+    solved = models.BooleanField(default=False)
+
